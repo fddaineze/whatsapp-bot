724d2ce6698af9e0a475cff7ea6fe1b03bd4a123
Filipe Daineze <filipe.daineze@outlook.com>
2023-04-28 14:41:15 -0300

    Update db.js

--- functions/db.js
+++ functions/db.js
@@ -23,11 +23,37 @@ const refreshData = () => {
     data = JSON.parse(fs.readFileSync(db));
 }
 
+const changeWhitelist = () => {
+    data.whitelistMode = !data.whitelistMode;
+    fs.writeFileSync(db, JSON.stringify(data));
+}
+const addblacklist = (id) => {
+    data.blacklist.push(id);
+    fs.writeFileSync(db, JSON.stringify(data));
+}
+const removeblacklist = (id) => {
+    data.blacklist = data.blacklist.filter(e => e !== id);
+    fs.writeFileSync(db, JSON.stringify(data));
+}
+const addwhitelist = (id) => {
+    data.whitelist.push(id);
+    fs.writeFileSync(db, JSON.stringify(data));
+}
+const removewhitelist = (id) => {
+    data.whitelist = data.blacklist.filter(e => e !== id);
+    fs.writeFileSync(db, JSON.stringify(data));
+}
+
 module.exports = {
     changePersonal: changePersonal,
     changeGroupOnly: changeGroupOnly,
     updateCount: updateCount,
     forceFileUpdate: forceFileUpdate,
     refreshData: refreshData,
+    changeWhitelist: changeWhitelist,
+    addblacklist: addblacklist,
+    removeblacklist: removeblacklist,
+    addwhitelist: addwhitelist,
+    removewhitelist: removewhitelist,
     data: data
 }
